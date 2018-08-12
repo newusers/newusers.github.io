640a43616fd431b3139221961b5b775ea69e36a5
Thuy Nguyen <newuser886@yahoo.com>
2018-08-12 23:40:31 +0700

    Update gulp

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,12 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 gulp.task('sass', function() {
-  return gulp.src('app/scss/styles.scss')
+  return gulp.src('app/scss/*.scss')
   .pipe(sass()) // Converts Sass to CSS with gulp-sass
   .pipe(gulp.dest('app/css'))
-});
+})
+
+gulp.task('watch', function() {
+  gulp.watch('app/scss/*.scss', gulp.parallel('sass'));
+  // Other watchers
+})
