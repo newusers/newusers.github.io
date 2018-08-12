2f57b23ca6ddf8fae62421d1abe513127dd7363c
Thuy Nguyen <newuser886@yahoo.com>
2018-08-12 21:35:14 +0700

    Update sass

---
+++ .bashrc
@@ -0,0 +1,1 @@
+export PATH="$PATH:/c/users/bient/desktop/gulp/node_modules/gulp/bin"
--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,10 @@
 var gulp = require('gulp');
 
-gulp.task('hello', function() {
-  console.log('Hello Thuy');
+// Requires the gulp-sass plugin
+var sass = require('gulp-sass');
+
+gulp.task('sass', function() {
+  return gulp.src('app/scss/styles.scss')
+  .pipe(sass()) // Converts Sass to CSS with gulp-sass
+  .pipe(gulp.dest('app/css'))
 });
