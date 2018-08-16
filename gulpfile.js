39390b10e12572e38f940202b6f09c5b84c988ce
Thuy Nguyen <newuser886@yahoo.com>
2018-08-16 23:09:11 +0700

    Update Browse syn

--- gulpfile.js
+++ gulpfile.js
@@ -3,13 +3,26 @@ var gulp = require('gulp');
 // Requires the gulp-sass plugin
 var sass = require('gulp-sass');
 
-gulp.task('sass', function() {
-  return gulp.src('app/scss/*.scss')
-  .pipe(sass()) // Converts Sass to CSS with gulp-sass
-  .pipe(gulp.dest('app/css'))
+var browserSync = require('browser-sync').create();
+
+gulp.task('sass', function () {
+    return gulp.src('app/scss/*.scss')
+        .pipe(sass()) // Converts Sass to CSS with gulp-sass
+        .pipe(gulp.dest('app/css'))
+        .pipe(browserSync.reload({
+            stream: true
+        }))
 })
 
-gulp.task('watch', function() {
-  gulp.watch('app/scss/*.scss', gulp.parallel('sass'));
-  // Other watchers
+gulp.task('browserSync', function () {
+    browserSync.init({
+        server: {
+            baseDir: 'app'
+        },
+    })
+})
+
+gulp.task('watch',['browserSync','sass'] function () {
+    gulp.watch('app/scss/*.scss', gulp.parallel('sass'));
+    // Other watchers
 })
